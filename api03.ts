a5b262527f3861ef6ca27af3274fa9e1803deebd
stonelouse <c.russ@web.de>
2020-10-16 06:35:36 +0200

    version 3: use unknown and type guard; fix api implementation

--- api03.ts
+++ api03.ts
@@ -1,12 +1,14 @@
 import { doTheMagic } from "./the-magic";
 import { FrozenError } from "./frozen-error";
-import { isUnfreezable } from "./unfreezable";
+import { isUnfreezable, Unfreezable } from "./unfreezable";
 
 export function freeze(toFreeze: unknown): unknown {
   // Introducing `unknown` we can't use the `unfreezable` property. We get a compile-time error!
   if (!isUnfreezable(toFreeze)) {
-    // at this point we know that `toFreeze` has the type `Unfreezable`
-    throw new FrozenError(toFreeze);
+    // at this point we know that `toFreeze` has NOT the type `Unfreezable`
+    return doTheMagic(toFreeze);
   }
-  return doTheMagic(toFreeze);
+  // at this point we know that `toFreeze` has the type `Unfreezable`
+  const unfreezable: Unfreezable = toFreeze;
+  throw new FrozenError(toFreeze);
 }
--- index.ts
+++ index.ts
@@ -5,9 +5,9 @@ import { freeze as api01_freeze } from "./api01";
 import { freeze as api02_freeze } from "./api02";
 import { freeze as api03_freeze } from "./api03";
 
-console.log(api02_freeze({ v: "api03" }));
-assertThrowsFrozenError({ v: "api03a", unfreezable: true }, api02_freeze);
-console.log(api02_freeze({ v: "api03b", unfreezable: false }));
+console.log(api03_freeze({ v: "api03" }));
+assertThrowsFrozenError({ v: "api03a", unfreezable: true }, api03_freeze);
+console.log(api03_freeze({ v: "api03b", unfreezable: false }));
 
 console.log(api02_freeze({ v: "api02" }));
 assertThrowsFrozenError({ v: "api02a", unfreezable: true }, api02_freeze);
